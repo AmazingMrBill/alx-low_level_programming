8da1d04b4dfd10d817a4855221776b07ae86188c
AmazingMrBill <williamsonbk47@gmail.com>
2022-06-02 02:44:34 -0700

    fun_work

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * printing the phrase with a new line
+ *    */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints the phrase and skips a line
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
