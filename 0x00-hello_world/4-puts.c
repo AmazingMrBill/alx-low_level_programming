c23cdfba00bffa44ab355b2abb110263f88120a5
AmazingMrBill <williamsonbk47@gmail.com>
2022-06-02 02:52:22 -0700

    j

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * printing the phrase with a new line
- *    */
+*printing the phrase with a new line
+*/
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }
